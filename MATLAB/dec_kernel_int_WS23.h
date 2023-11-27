d7e625e186871fa06f9d02732d9169f926617434
lennard <lennard.joensson@haw-hamburg.de>
2023-11-27 19:05:12 +0100

    um den Kernel-Filter erweitert

---
+++ MATLAB/dec_kernel_int_WS23.h
@@ -0,0 +1,10 @@
+//------------------------------------------- 
+// designed with -- dec_kernel_int.m -- 
+// Fs = 50000.00
+// MMmin =   4.00
+// fstop = 3350.00
+// fpass = 3100.00
+// delta_pass =   0.01
+// delta_stop_dB =  40.00
+// N_FIR = 391, instead two stages with N_FIR_Dec_Int = 18, N_FIR_KERNEL = 116
+//------------------------------------------- 
