7541b1f8e8f53a6237ec219700ce3824536c31de
acp441 <45313740+acp441@users.noreply.github.com>
2024-01-09 10:39:40 +0100

    SRC Labor

---
+++ adda_SRC_Lab5/Debug/adda.pp
@@ -0,0 +1,33 @@
+# FIXED
+
+adda.obj: C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/adda.c
+adda.obj: ../../../../BoardSupport/inc/unidaq2.h
+adda.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h
+adda.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
+adda.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdint.h
+adda.obj: ../../../../BoardSupport/inc/errors.h
+adda.obj: C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/adda.h
+adda.obj: C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/i2c.h
+adda.obj: ../../../../ti/pspiom/cslr/soc_C6747.h
+adda.obj: ../../../../ti/pspiom/cslr/cslr.h
+adda.obj: ../../../../ti/pspiom/cslr/cslr_psc_C6747.h
+adda.obj: ../../../../ti/pspiom/cslr/tistdtypes.h
+adda.obj: ../../../../ti/pspiom/cslr/cslr_mcasp.h
+adda.obj: ../../../../ti/pspiom/cslr/cslr_edma3cc.h
+adda.obj: ../../../../ti/pspiom/cslr/cslr_tmr.h
+
+C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/adda.c: 
+../../../../BoardSupport/inc/unidaq2.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdint.h: 
+../../../../BoardSupport/inc/errors.h: 
+C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/adda.h: 
+C:/UniDAQ2.DSP-ADDA/BoardSupport/drv/i2c.h: 
+../../../../ti/pspiom/cslr/soc_C6747.h: 
+../../../../ti/pspiom/cslr/cslr.h: 
+../../../../ti/pspiom/cslr/cslr_psc_C6747.h: 
+../../../../ti/pspiom/cslr/tistdtypes.h: 
+../../../../ti/pspiom/cslr/cslr_mcasp.h: 
+../../../../ti/pspiom/cslr/cslr_edma3cc.h: 
+../../../../ti/pspiom/cslr/cslr_tmr.h: 
